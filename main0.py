5e6e8ed8c7f5f2821c868aa40170a47d7dfedba5
Iippa <iiro.peltola@gmail.com>
2017-04-03 18:38:49 +0300

    Kivy added

--- main0.py
+++ main0.py
@@ -1,14 +1,3 @@
-
-import binascii
-import sys
-import Adafruit_PN532 as PN532
-import kivy
-
-from kivy.app import App
-from kivy.uix.label import Label
-
-kivy.require('1.9.1') # replace with your current kivy version !
-
 from kivy.app import App
 from kivy.properties import StringProperty
 from kivy.uix.screenmanager import ScreenManager, Screen, SlideTransition
@@ -110,62 +99,15 @@ codes = {
 
 ##Read value from NFC/RFID reader
 print "Please insert UID"
-scan = uid
+scan = raw_input()
 print scan
 
-# Setup how the PN532 is connected to the Raspbery Pi/BeagleBone Black.
-# It is recommended to use a software SPI connection with 4 digital GPIO pins.
-
-# Configuration for a Raspberry Pi:
-CS   = 18
-MOSI = 23
-MISO = 24
-SCLK = 25
-
-# Configuration for a BeagleBone Black:
-# CS   = 'P8_7'
-# MOSI = 'P8_8'
-# MISO = 'P8_9'
-# SCLK = 'P8_10'
-
-# Create an instance of the PN532 class.
-pn532 = PN532.PN532(cs=CS, sclk=SCLK, mosi=MOSI, miso=MISO)
-# Call begin to initialize communication with the PN532.  Must be done before
-# any other calls to the PN532!
-pn532.begin()
-# Configure PN532 to communicate with MiFare cards.
-pn532.SAM_configuration()
-
-#Create toggle switch to represent succesfull opening of lock
-
-key = False
-
-# Kivy App
-class MyApp(App):
-        def build(self):
-            return Label(text='Tervetuloa %s' %name)
-def scan_database(scan):
+while(1):
     #Search through all know tags and print result
     for code in codes:
+        name = ''
+        global name
         if scan == codes[code]['info']['tag']:
-            return True
-
-def valid_login():
-        #Read value from NFC/RFID reader
-        scan = '0x{0}'.format(binascii.hexlify(uid))
-        if scan_database():
-            return True
-        else:
-            return False
-
-print ('Waiting for Mifare card...')
-while(1):
-        uid = pn532.read_passive_target()
-        if uid is None:
-                continue
-        if valid_login():
             CarouselApp().run()
-        else:
-            LoginApp().run()
-        print ('Found card with UID: 0x{0}'.format(binascii.hexlify(uid)))
-        break
+    if Key == False:
+        LoginApp().run()
