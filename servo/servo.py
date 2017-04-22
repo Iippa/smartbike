c775b265438ccb74a0de0b269ee5699dd8c96980
Iippa <iiro.peltola@gmail.com>
2017-04-22 12:14:33 +0300

    fixes

--- servo/servo.py
+++ servo/servo.py
@@ -5,6 +5,7 @@ GPIO.setup(servoPin,GPIO.OUT)
 pwm=GPIO.PWM(servoPin,50)
 pwm.start(7)
 for i in range(0,20):
+    input("Move")
     desiredPosition = 0
     DC=1./18.*(desiredPosition)+2
     pwm.ChangeDutyCycle(DC)
