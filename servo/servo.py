0877c6f014a7a14d5ae5b813e681299297d933ba
Iippa <iiro.peltola@gmail.com>
2017-04-22 12:16:02 +0300

    fixes

--- servo/servo.py
+++ servo/servo.py
@@ -5,13 +5,16 @@ GPIO.setup(servoPin,GPIO.OUT)
 pwm=GPIO.PWM(servoPin,50)
 pwm.start(7)
 for i in range(0,20):
-    input("Move")
-    desiredPosition = 0
-    DC=1./18.*(desiredPosition)+2
-    pwm.ChangeDutyCycle(DC)
-    time.sleep(1)
-    desiredPosition = 80
-    DC=1./18.*(desiredPosition)+2
-    pwm.ChangeDutyCycle(DC)
+    userinput = input("Move")
+    if userinput == 1:
+        desiredPosition = 0
+        DC=1./18.*(desiredPosition)+2
+        pwm.ChangeDutyCycle(DC)
+        time.sleep(1)
+        desiredPosition = 80
+        DC=1./18.*(desiredPosition)+2
+        pwm.ChangeDutyCycle(DC)
+    else:
+        continue
 pwm.stop()
 GPIO.cleanup()
