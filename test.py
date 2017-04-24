2529cac48f1e4a357902818edae7c88e57a4c2aa
Iippa <iiro.peltola@gmail.com>
2017-04-24 10:15:34 +0300

    Fixes

--- test.py
+++ test.py
@@ -103,7 +103,8 @@ class Bike(Button):
         togle_servo(3)
         #Turn screen backlight off
         os.system("echo"+" "+"1"+" "+">"+"/sys/class/backlight/rpi_backlight/bl_power")
-        RootApp.exit()
+        clear_widgets()
+        RootApp.stop()
 
 
 def togle_servo(time):
